30368a0c6f2015458ad007a03370e4fae805da6f
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-02-10 23:08:20 +0530

    Added banner and list of rows

--- src/screens/components/Navbar/Nav.jsx
+++ src/screens/components/Navbar/Nav.jsx
@@ -37,7 +37,3 @@ export default function Nav() {
     </div>
   );
 }
-
-// Netflix logo: http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png
-
-// avatar: https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png
