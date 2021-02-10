4d509f876d59cafc489e19dd4dce14e7fdc2e5f7
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-02-10 23:03:34 +0530

    Added Banner and list of rows

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -15,33 +15,13 @@ export default function HomeScreen() {
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow />
-      <Row
-        title="Action Movies"
-        fetchUrl={requests.fetchActionMovies}
-        isLargeRow
-      />
-      <Row
-        title="Comedy Movies"
-        fetchUrl={requests.fetchComedyMovies}
-        isLargeRow
-      />
-      <Row
-        title="Horror Movies"
-        fetchUrl={requests.fetchHorrorMovies}
-        isLargeRow
-      />
-      <Row
-        title="Romance Movies"
-        fetchUrl={requests.fetchRomanceMovies}
-        isLargeRow
-      />
-      <Row
-        title="Documentaries"
-        fetchUrl={requests.fetchDocumentaries}
-        isLargeRow
-      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/screens/components/Navbar/Nav.jsx
+++ src/screens/components/Navbar/Nav.jsx
@@ -1,45 +1,43 @@
-import React, {useEffect, useState} from 'react'
-import './Nav.css';
-
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
 
 export default function Nav() {
+  const [show, setShow] = useState(false);
 
-    const [show, setShow] = useState(false);
-    
-    const transitionHandler = () => {
-        if(window.scrollY > 100){
-            setShow(true);
-        }else{
-            setShow(false);
-        }
+  const transitionHandler = () => {
+    if (window.scrollY > 100) {
+      setShow(true);
+    } else {
+      setShow(false);
     }
+  };
 
-    useEffect(() => {
-        window.addEventListener('scroll', transitionHandler);
-        return () => {
-            window.removeEventListener('scroll', transitionHandler);
-        }
-    }, [])
+  useEffect(() => {
+    window.addEventListener("scroll", transitionHandler);
+    return () => {
+      window.removeEventListener("scroll", transitionHandler);
+    };
+  }, []);
 
-    return (
-        <div className = {`nav ${show && "nav__black"}`}>
-            <div className="nav__contents">
-                <img
-                className = "nav__logo" 
-                 src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-                 alt="netfilx-logo"/>
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="netfilx-logo"
+        />
 
-                 <img 
-                 className = "nav__avatar"
-                 src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" 
-                 alt="avatar"/>
-            </div>
-        </div>
-    )
+        <img
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
 }
 
-
 // Netflix logo: http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png
-
 
 // avatar: https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png
