57c45390579aec7321ab0473389759cd602c5fe7
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-02-10 18:45:55 +0530

    initial commit

---
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import requests from "../../app/Requests";
+import Banner from "../components/Banner/Banner";
+import Nav from "../components/Navbar/Nav";
+import Row from "../components/Row/Row";
+
+export default function HomeScreen() {
+  return (
+    <div className="homeScreen">
+      <Nav />
+      <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow />
+      <Row
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+        isLargeRow
+      />
+      <Row
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+        isLargeRow
+      />
+      <Row
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+        isLargeRow
+      />
+      <Row
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+        isLargeRow
+      />
+      <Row
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+        isLargeRow
+      />
+    </div>
+  );
+}
---
+++ src/screens/components/Row/Row.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Row.css";
+
+export default function Row() {
+  return (
+    <div className="row">
+      <h2>This is a title</h2>
+    </div>
+  );
+}
